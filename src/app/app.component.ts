0db75718c71c001b548f21682a0d0d8db5196b1e
Aman Yadav <amanyddv@gmail.com>
2024-02-21 00:10:10 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, map } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,121 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firestore';
+
+  constructor(private firestore: AngularFirestore) {}
+  tutorials: Observable<any[]> | undefined;
+
+  users: Observable<any[]> | undefined;
+
+  collectionData: Observable<any[]> | undefined;
+
+  
+  ngOnInit() {
+    // this.firestore.collection('myCollection').doc('myDoc').valueChanges().subscribe(data => {
+    //   console.log(data)
+    // });
+    const data = {
+      name: 'aman',
+      age: 30,
+      city: 'New York'
+    };
+
+
+    this.firestore.collection('users').add(data);
+
+    // this.firestore.collection('users').doc('my-user-id').set(data);
+
+    
+    // this.firestore.collection('users').add(data)
+    //   .then(() => {
+    //     console.log('Data saved successfully!');
+    //   })
+    //   .catch(error => {
+    //     console.error('Error saving data:', error);
+    //   });
+
+    // console.log(data)
+
+
+    
+
+    // this.tutorials = this.firestore.collection('users').snapshotChanges()
+    // console.log(this.tutorials)
+    // this.tutorials.pipe(
+    //   map(changes =>
+        
+    //     changes.map((c: { payload: { doc: { id: any; data: () => any; }; }; }) =>
+    //       ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+    //     )
+    //   )
+    // ).subscribe(data => {
+    //   console.log(data);
+    // });
+
+
+
+    // this.firestore.collection('users').get()
+    // .subscribe(querySnapshot => {
+    //   querySnapshot.forEach((doc) => {
+    //     console.log(doc)
+    //   });
+
+    //   console.log('The users collection was deleted successfully!');
+    // }, error => {
+    //   console.error('Error deleting users collection:', error);
+    // });
+
+
+
+    this.collectionData = this.firestore.collection('users').valueChanges();
+    this.collectionData.subscribe(data => {
+      console.log(data); // Log the data after it's received
+    });
+
+    console.log("id")
+
+    this.tutorials = this.firestore.collection('users').snapshotChanges();
+    this.tutorials.subscribe(data => {
+      console.log(data); // Log the data after it's received
+    });
+    
+    this.users = this.firestore.collection('users').snapshotChanges();
+
+
+
+
+    const newData = {
+      // Define the updated data for the user
+      // For example, if you have a 'name' field, you can update it like this:
+      name: 'Updated Name'
+    };
+
+    this.firestore.collection('users').doc('LstqP7OuarsiYD6uFN9z').update(newData)
+      .then(() => {
+        console.log(`User with ID ${ 'LstqP7OuarsiYD6uFN9z'} updated successfully.`);
+      })
+      .catch(error => {
+        console.error(`Error updating user with ID ${ 'LstqP7OuarsiYD6uFN9z'}:`, error);
+      });
+
+
+      
+   
+  }
+
+
+
+  deleteUser(userId: string) {
+    this.firestore.collection('users').doc(userId).delete()
+      .then(() => {
+        console.log(`User with ID ${userId} deleted successfully.`);
+      })
+      .catch(error => {
+        console.error(`Error deleting user with ID ${userId}:`, error);
+      });
+    }
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({"projectId":"temp-1a793","appId":"1:633294722727:web:8d8d5eb80ea1a3df382d82","storageBucket":"temp-1a793.appspot.com","apiKey":"AIzaSyCSP0dgBoc4UNs9JyHwOK2zNHtXhwygpUY","authDomain":"temp-1a793.firebaseapp.com","messagingSenderId":"633294722727"}),
+    AngularFirestoreModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
